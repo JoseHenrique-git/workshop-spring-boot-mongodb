c0479c009dcefdf6e9b87b89e086114f94c807b1
JoseHenrique-git <silveiraecarvalho29@gmail.com>
2025-12-03 16:45:07 -0300

    Atualização de usuário com PUT

--- src/main/java/com/josehenrique/workshopmongo/services/UserService.java
+++ src/main/java/com/josehenrique/workshopmongo/services/UserService.java
@@ -38,6 +38,18 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User upDate(User obj) {
+		//User newObj = repo.findOne(obj.getId());
+		User newObj = repo.findById(obj.getId()).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
