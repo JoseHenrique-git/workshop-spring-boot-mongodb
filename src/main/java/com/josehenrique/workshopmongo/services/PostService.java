0274fa9f8f324a22ec986eae912a62c9b44f6734
JoseHenrique-git <silveiraecarvalho29@gmail.com>
2025-12-06 15:25:57 -0300

    Obtendo um post por id

---
+++ src/main/java/com/josehenrique/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.josehenrique.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.josehenrique.workshopmongo.domain.Post;
+import com.josehenrique.workshopmongo.repository.PostRepository;
+import com.josehenrique.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}	
+}
